71b0d49de7c28d28d80d86a1bcef961813182fc6
Bas Kardinaal <bkardinaal@trumanscout.com>
2018-02-01 09:09:21 +0100

    partials

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-// Alert remove
-btnClose = alert.querySelector('.close');
+// // Alert remove
+const alert = document.querySelector('.alert');
+const btnClose = alert.querySelector('.close');
 
 btnClose.addEventListener('click', function() {
     btnClose.parentNode.remove();
