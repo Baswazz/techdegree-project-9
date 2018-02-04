319e267225c056cab25e76ccf3b8426536959fa4
Bas Kardinaal <baswazz@gmail.com>
2018-02-04 19:53:54 +0100

    partials

--- js/app.js
+++ js/app.js
@@ -1,7 +1,43 @@
-// // Alert remove
-const alert = document.querySelector('.alert');
+// Alert remove
+const alert    = document.querySelector('.alert');
 const btnClose = alert.querySelector('.close');
 
-btnClose.addEventListener('click', function() {
+btnClose.addEventListener('click', function(e) {
     btnClose.parentNode.remove();
+});
+
+// Dropdown toggle
+const dropdown        = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdown.addEventListener('click', function(e) {
+    dropdownContent.parentNode.classList.toggle('dropdown-show');
+});
+
+// Tabs
+const tabs       = document.querySelector('.tabs');
+const tabContent = document.querySelectorAll('.tab-content');
+
+tabs.addEventListener('click', function(e) {
+    if (e.target.tagName === 'A') {
+        e.preventDefault();
+
+        anchors = tabs.getElementsByTagName('a');
+        for (i = 0; i < anchors.length; i ++) {
+            anchors[i].classList.remove('active');
+        }
+        
+        e.target.classList.add('active')
+        const tabHref = e.target.getAttribute('href');
+
+        for (i = 0; i < tabContent.length; i ++) {
+            const tabId = '#' + tabContent[i].id;
+            tabContent[i].classList.remove('show');
+
+            if (tabId === tabHref) {
+                tabContent[i].classList.add('show');
+                // console.log('yeahhhh!');
+            }
+        }
+    }
 });
